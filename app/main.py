45a3de48f60bcd3a85a26f05f32bddd635acff80
hashtag-squirrel <alita2000@web.de>
2026-02-11 21:00:20 +0100

    Update fastapi to work with a sqlite db

--- app/main.py
+++ app/main.py
@@ -1,28 +1,63 @@
-from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr
+from typing import Annotated
+from fastapi import Depends, FastAPI, HTTPException, Query
+from pydantic import EmailStr
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-app = FastAPI()
 
-
-class Feedback(BaseModel):
+class Feedback(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
     name: str
     email: EmailStr
     message: str
 
 
-@app.get("/feedback/{feedback_id}")
-def read_feedback(feedback: Feedback):
-    return {
-        'name': feedback.name,
-        'email': feedback.email,
-        'message': feedback.message
-    }
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 
 @app.post("/feedback/")
-def create_feedback(feedback: Feedback):
-    return {
-        'name': feedback.name,
-        'email': feedback.email,
-        'message': feedback.message
-    }
+def create_feedback(feedback: Feedback, session: SessionDep):
+    session.add(feedback)
+    session.commit()
+    session.refresh(feedback)
+    return feedback
+
+
+@app.get("/feedback/")
+def read_feedbacks(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[Feedback]:
+    feedback = session.exec(select(Feedback).offset(offset).limit(limit)).all()
+    return feedback
+
+
+@app.get("/feedback/{feedback_id}")
+def read_feedback(feedback_id: int, session: SessionDep) -> Feedback:
+    feedback = session.get(Feedback, feedback_id)
+    if not feedback:
+        raise HTTPException(status_code=404, detail="Feedback not found")
+    return feedback
